b6c01d288abe9a5162b420fbb5cad982353abbf8
Vianne Buinguyen <vianneb@uw.edu>
2022-06-14 13:58:52 -0700

    add projects section with images and hover animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,25 @@
 import React from "react";
 
 export function About() {
-    return(
+    return (
         <section className="container mb-5">
             <h2 className="heading">ABOUT</h2>
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-8">
 
-                    
-                    <p>
-                        I'm a junior majoring in <a className="about-link" href="https://ischool.uw.edu/programs/informatics">Informatics</a> at the <a className="about-link" href="https://ischool.uw.edu/">University of Washington iSchool</a>.
-                        I'm most drawn to product management and learning how to create accessible, inclusive, and insipiring technology-powered products that people love.
-                    </p>
+                        <p>
+                            I'm a junior majoring in <a className="about-link" href="https://ischool.uw.edu/programs/informatics">Informatics</a> at the <a className="about-link" href="https://ischool.uw.edu/">University of Washington iSchool</a>.
+                            I'm most drawn to product management and learning how to create accessible, inclusive, and insipiring technology-powered products that people love.
+                        </p>
 
-                    <p>
-                        When I'm not desiging, developing, or leading, you can catch me playing with my Australian Shepherd puppy, Koda, or on a mission to find the best-tasting boba in Seattle with my friends!
-                    </p>
+                        <p>
+                            When I'm not desiging, developing, or leading, you can catch me playing with my Australian Shepherd puppy, Koda, or on a mission to find the best-tasting boba in Seattle with my friends!
+                        </p>
 
-                    <p>
-                        I love meeting new people - say hi at <a className="about-link" href="mailto: vianneb@uw.edu">vianneb@uw.edu</a>!
-                    </p>
+                        <p>
+                            I love meeting new people - say hi at <a className="about-link" href="mailto: vianneb@uw.edu">vianneb@uw.edu</a>!
+                        </p>
 
                     </div>
                 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
 export function Projects() {
-    return(
+    return (
         <section className='container mb-5'>
             <h2 className='heading'>SELECTED PROJECTS</h2>
+            <div className='container'>
+                <div className=' d-flex row'>
+                    <div className='col-12 col-md-6 mb-2'>
+                        <div className='card image'>
+                            <img className='image__img' src="img/dubmatch.png" alt="prototypes for DubMatch app"></img>
+
+                            <div className='image__overlay'>
+                                <h3 className='image__title'>DubMatch</h3>
+                                <p className='image__description ms-2 me-2'>A mobile application that connects users to classmates of similar interests.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 mb-2'>
+                        <div className='card image'>
+                            <img className='image__img' src="img/foody.png" alt="prototype for Foody app"></img>
+
+                            <div className='image__overlay'>
+                                <h3 className='image__title'>Foody</h3>
+                                <p className='image__description ms-2 me-2'>An interactive web application that allows users to search, view, share, and save local Seattle restuarants.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 mb-2'>
+                        <div className='card image'>
+                            <img className='image__img' src="img/livefresh.png" alt="prototypes for LiveFresh app"></img>
+
+                            <div className='image__overlay'>
+                                <h3 className='image__title'>LiveFresh</h3>
+                                <p className='image__description ms-2 me-2'>A mobile application that allows users to track the expiration dates of items in their fridge and find recipes for expiring items.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 mb-2'>
+                        <div className='card image'>
+                            <img className='image__img' src="img/min_wage.png" alt="prototype for Analyzing US Minimum Wages Shiny app"></img>
+                            
+                            <div className='image__overlay'>
+                                <h3 className='image__title'>Analyzing US Minimum Wages</h3>
+                                <p className='image__description ms-2 me-2'>A web application with interactive data visualizations that explore trends in US federal and state minimum wages over time.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
